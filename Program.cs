cfe6931cf111f084adbb6f57321d1485324ca621
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:24:15 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿foreach (var item in Enum.GetNames(typeof(Days)))
+{
+    System.Console.WriteLine(item);
+}
+Days a = (Days)4;
+Console.WriteLine(a);
+enum Days{
+    Lundi,
+    Mardi,
+    Mercredi,
+    Jeudi,
+    Vendredi,
+    Samedi,
+    Dimanche
+};
+
