4c45bff022404b948e490919ca2b9a4164fed71c
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-16 11:16:36 +0100

    Modifié l'exercice selon l'énoncé. Amélioré l'affichage pour l'utilisatuer et le commentaire pour le développeur.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿foreach (var item in Enum.GetNames(typeof(Days)))
+﻿// Affichage de toutes les valeurs de l'enum
+Console.WriteLine("Voici toutes les valeurs de l'enum week :");
+foreach (var item in Enum.GetNames(typeof(week)))
 {
     System.Console.WriteLine(item);
 }
-Days a = (Days)4;
-Console.WriteLine(a);
-enum Days{
+// Affichage de la valeur 4 de l'enum
+week valueDay = (week)3;
+Console.WriteLine($"Voici la valeur 4 de l'enum week{valueDay}");
+enum week{
     Lundi,
     Mardi,
     Mercredi,
